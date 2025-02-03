c8d3620dc8f7ebe61f90839e732136e05fd8641c
Ivan Clavijo <ivan.clavijo2@gmail.com>
2025-02-03 04:52:04 +0200

    fixed video

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-    <section className="relative mb-8 h-[60vh] overflow-hidden">
+    <div className="relative h-screen w-full overflow-hidden">
       <video
         className="absolute left-0 top-0 h-full w-full object-cover"
         autoPlay
@@ -8,18 +8,17 @@ export default function Hero() {
         muted
         playsInline
       >
-        {/*<source src="../videos/knessetVideo.mp4" type="video/webm" />*/}
         <source src="/videos/knessetVideo.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <div className="absolute left-0 top-0 flex h-full w-full flex-col items-center justify-center bg-black bg-opacity-50 text-center text-white">
+      {/*<div className="absolute left-0 top-0 flex h-full w-full flex-col items-center justify-center bg-black bg-opacity-50 text-center text-white">
         <h1 className="mb-4 text-4xl font-bold md:text-5xl">
           Have Your Say on the Latest Knesset Bills!
         </h1>
         <p className="text-xl md:text-2xl">
           Browse proposed laws, vote, and share your thoughts.
         </p>
-      </div>
-    </section>
+      </div>*/}
+    </div>
   );
 }
